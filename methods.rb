5c8ef262b680f1d99c07bc66fac36e68815b635d
rose534 <wambuirose534@gmail.com>
2023-02-07 17:22:34 +0300

    lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
-# Your code here!
+
+def greet_programmer 
+    puts "Hello, programmer!"
+  end
+
+  def greet (name = Naureen)
+    puts "Hello, #{name}!"
+  end
+  
+  def greet_with_default (name = Naureen)
+    puts "Hello, #{name}!"
+  end 
+
+
+  def greet_with_default (name = Naureen)
+    puts "Hello, #{name}! when called with #{name}"
+    
+    end
+
+
+  def add (sum1, sum2)
+    return sum1 + sum2
+ 
+ end
+ add(2,5)
+
+  def halve (sum3)
+    return sum3 / 2
+   
+  end 
+
+  halve (6)
+
+
